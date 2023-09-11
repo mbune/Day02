fab0a711984d98c7ca5cae494ea8727a59cce8f8
Daniel Keefe <dfk@umn.edu>
2023-09-11 14:40:16 -0500

    work during class

--- src/ExampleApp.ts
+++ src/ExampleApp.ts
@@ -20,6 +20,20 @@ export class ExampleApp extends gfx.GfxApp
     createScene(): void 
     {
         this.renderer.background = new gfx.Color(0.226, 0.450, 0.610);
+
+        this.scene.add(gfx.Geometry2Factory.createBox(0.2, 0.2));
+
+        const rect1 = gfx.Geometry2Factory.createBox(0.2, 0.2);
+        this.scene.add(rect1);
+        rect1.position = new gfx.Vector2(-0.5, 0.5);
+
+        const rect2 = gfx.Geometry2Factory.createBox(0.2, 0.2);
+        this.scene.add(rect2);
+        rect2.position = new gfx.Vector2(0.5, 0.5);
+
+        const mouth = gfx.Geometry2Factory.createBox(1.2, 0.3);
+        this.scene.add(mouth);
+        mouth.position = new gfx.Vector2(0, -0.5);
     }
 
 
